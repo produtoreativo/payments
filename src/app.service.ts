08dabc40d4fea9655f7a871fd82951f999925eea
Christiano Milfont <cmilfont@gmail.com>
2021-06-15 11:01:21 -0300

    Fix sonar

--- src/app.service.ts
+++ src/app.service.ts
@@ -33,8 +33,7 @@ export class AppService {
     const dto = invoice.createDTO();
     const providerPayload = await starkbank.invoice.create([dto]);
     invoice.setProvider(providerPayload);
-    await this.invoiceRepository.save(invoice);
-    return invoice;
+    return await this.invoiceRepository.save(invoice);
   }
 
 }
