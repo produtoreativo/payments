2f947c76e6ff3c3e5e2d326e9392ca16256929f5
Christiano Milfont <cmilfont@gmail.com>
2021-06-15 17:03:43 -0300

    fix

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,6 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
-import * as ormconfig from '../ormconfig';
-
 @Module({
   controllers: [AppController],
   providers: [AppService],
