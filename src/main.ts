32349bdb7aa24f15a553619b3d94760506cdf529
Christiano Milfont <cmilfont@gmail.com>
2021-06-16 11:08:33 -0300

    colocando a porta como o heroku gosta

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
+import * as dotenv from "dotenv";
+dotenv.config({ path: process.env.DOTENV_PATH || undefined });
+
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await app.listen(process.env.PORT, '0.0.0.0');
 }
 bootstrap();
