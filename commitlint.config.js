4cca5e4f09e26d593beac9452180a6295de6c37a
Christiano Milfont <cmilfont@gmail.com>
2021-06-25 16:34:13 -0300

    feat: cI Async

--- commitlint.config.js
+++ commitlint.config.js
@@ -7,34 +7,6 @@ module.exports = {
     'jira-task-id-min-length': [0],
     'jira-commit-status-case': [0],
     'header-max-length': [0],
-    'jira-task-id-max-length': [0],
-    'scope-case': [
-      2,
-      'always',
-      [
-        'lower-case', // default
-        'upper-case', // UPPERCASE
-        'camel-case', // camelCase
-        'kebab-case', // kebab-case
-        'pascal-case', // PascalCase
-        'sentence-case', // Sentence case
-        'snake-case', // snake_case
-        'start-case' // Start Case
-      ]
-    ],
-    'subject-case': [
-      2,
-      'always',
-      [
-        'lower-case', // default
-        'upper-case', // UPPERCASE
-        'camel-case', // camelCase
-        'kebab-case', // kebab-case
-        'pascal-case', // PascalCase
-        'sentence-case', // Sentence case
-        'snake-case', // snake_case
-        'start-case' // Start Case
-      ]
-    ]
+    'jira-task-id-max-length': [0]
   }
 }
