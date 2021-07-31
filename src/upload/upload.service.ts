7314448497963cbd08ab6966e1907dd86e1b9840
Christiano Milfont <cmilfont@gmail.com>
2021-07-31 09:28:09 -0300

    feat(#0.0.3): Criando service para Upload

--- src/upload/upload.service.ts
+++ src/upload/upload.service.ts
@@ -3,26 +3,26 @@ import { InjectS3, S3 } from 'nestjs-s3';
 
 @Injectable()
 export class UploadService {
-  constructor(
-    @InjectS3() private readonly s3: S3,
-  ) {}
+  constructor(@InjectS3() private readonly s3: S3) {}
 
   async sendJSON(id, provider, json) {
-  
     const data = {
-      Key: `${provider}-${id}`, 
+      Key: `${provider}-${id}`,
       Body: JSON.stringify(json),
       ContentType: 'application/json',
       Bucket: 'prodops-payments',
-      ACL: 'public-read'
+      ACL: 'public-read',
     };
 
-    const response: any = await this.s3.putObject(data).promise().catch(err => {
-      console.log('Erro aws', err)
-    });
+    const response: any = await this.s3
+      .putObject(data)
+      .promise()
+      .catch((err) => {
+        console.log('Erro aws', err);
+      });
 
     const { Location, Key } = response;
-    console.log(Location, Key)
+    console.log(Location, Key);
     return response;
   }
 }
